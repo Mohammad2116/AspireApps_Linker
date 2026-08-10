b299f0ce733371a90fbdc3a7a405203d2779fa40
Mohammad Momensafaie <Mohammad2116@gmail.com>
2026-08-10 14:47:16 +0330

    feat: gateway configuration comlete and also JwtAuthenticationFilter sets to recive requests
    Signed-off-by: Mohammad Momensafaie <Mohammad2116@gmail.com>

--- gateway-server/src/main/java/ir/aspireapps/linker/gatewayserver/config/JwtAuthenticationFilter.java
+++ gateway-server/src/main/java/ir/aspireapps/linker/gatewayserver/config/JwtAuthenticationFilter.java
@@ -1,12 +1,16 @@
 package ir.aspireapps.linker.gatewayserver.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
