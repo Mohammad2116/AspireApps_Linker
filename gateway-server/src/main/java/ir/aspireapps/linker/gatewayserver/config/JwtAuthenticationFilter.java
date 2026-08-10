c392f7ed00dbd21ceec15acd7786d980b3fde450
Mohammad Momensafaie <Mohammad2116@gmail.com>
2026-08-10 15:15:47 +0330

    feat: gateway implementation complete, and added a Test controller to check availablity of service - After adding each microservice there is a minor update for gateway
    Signed-off-by: Mohammad Momensafaie <Mohammad2116@gmail.com>

--- gateway-server/src/main/java/ir/aspireapps/linker/gatewayserver/config/JwtAuthenticationFilter.java
+++ gateway-server/src/main/java/ir/aspireapps/linker/gatewayserver/config/JwtAuthenticationFilter.java
@@ -1,25 +1,87 @@
 package ir.aspireapps.linker.gatewayserver.config;
 
+import io.jsonwebtoken.Claims;
+import ir.aspireapps.linker.gatewayserver.service.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
+    private static final List<String> PUBLIC_PATHS = List.of(
+            "/ir/aspireapps/api/v1/...",
+            "/actuator",
+            "/ir/aspireapps/api/v1/linker/gateway/anything"
+    );
+    private static final List<String> ADMIN_PATHS = List.of(
+            "/ir/aspireapps/api/v1/linker/admin/**"
+    );
+    private final JwtService jwtService;
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        exchange.getResponse().setStatusCode(HttpStatus.OK);
-        return exchange.getResponse().setComplete();
+
+        String path = exchange.getRequest().getURI().getPath();
+        if (isPublic(path)) {
+            return chain.filter(exchange);
+        }
+
+        String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return unauthorized(exchange);
+        }
+
+        String token = authHeader.substring("Bearer ".length());
+        Claims claims = jwtService.validateToken(token);
+        String username = claims.getSubject();
+        List<?> roles = claims.get("roles", List.class);
+        if (username == null || roles == null) {
+            return unauthorized(exchange);
+        }
+        List<String> rolesNames = roles.stream().map(Object::toString).toList();
+        if (isAdmin(path) && !rolesNames.contains("ROLE_ADMIN")) {
+            return forbidden(exchange);
+        }
+
+        ServerHttpRequest request = exchange
+                .getRequest()
+                .mutate()
+                .header("X-USERNAME", username)
+                .header("X-USER-ROLES", String.join(",", rolesNames))
+                .build();
+        return chain.filter(exchange.mutate().request(request).build());
     }
 
     @Override
     public int getOrder() {
         return -1;
+    }
+
+    private boolean isPublic(String path) {
+        return PUBLIC_PATHS.stream().anyMatch(path::startsWith);
+    }
+
+    private boolean isAdmin(String path) {
+        return ADMIN_PATHS.stream().anyMatch(path::startsWith);
+    }
+
+    private Mono<Void> forbidden(ServerWebExchange exchange) {
+        exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
+        return exchange.getResponse().setComplete();
+    }
+
+    private Mono<Void> unauthorized(ServerWebExchange exchange) {
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
     }
 }
---
+++ gateway-server/src/main/java/ir/aspireapps/linker/gatewayserver/controller/GatewayTestController.java
@@ -0,0 +1,17 @@
+package ir.aspireapps.linker.gatewayserver.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GatewayTestController {
+    @GetMapping("/ir/aspireapps/api/v1/linker/gateway/anything")
+    public ResponseEntity<String> anything() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Gateway Test Success, gateway working correctly...");
+    }
+
+}
