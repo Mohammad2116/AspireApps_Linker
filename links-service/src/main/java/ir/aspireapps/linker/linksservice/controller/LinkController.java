591fb0c19a89ca5a3bc6383536a7f7863abfec74
Mohammad Momensafaie <Mohammad2116@gmail.com>
2026-08-15 14:47:30 +0330

    feat: LinkService endpoint for accessing links done, some minor updates for gateway jwt filter done to make it save current logged in userID in headers
    Signed-off-by: Mohammad Momensafaie <Mohammad2116@gmail.com>
    
    # Conflicts:
    #	links-service/src/main/java/ir/aspireapps/linker/linksservice/controller/LinkController.java
    #	links-service/src/main/java/ir/aspireapps/linker/linksservice/repository/LinkRepository.java
    #	links-service/src/main/java/ir/aspireapps/linker/linksservice/service/LinkService.java

---
+++ links-service/src/main/java/ir/aspireapps/linker/linksservice/controller/LinkController.java
@@ -0,0 +1,72 @@
+package ir.aspireapps.linker.linksservice.controller;
+
+import ir.aspireapps.linker.linksservice.dto.LinkRegisterRequest;
+import ir.aspireapps.linker.linksservice.dto.LinkResponse;
+import ir.aspireapps.linker.linksservice.dto.LinkUpdateStatusRequest;
+import ir.aspireapps.linker.linksservice.service.LinkService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.ws.rs.HeaderParam;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/ir/aspireapps/linker/links/api/v1/**")
+@RequiredArgsConstructor
+public class LinkController {
+    private final LinkService linkService;
+
+    @PostMapping("register")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    public ResponseEntity<LinkResponse> register(
+            @Valid @NotNull @RequestBody LinkRegisterRequest request,
+            @NotEmpty @HeaderParam("X-USER-ID") String userId) {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(
+                        linkService.register(request, UUID.fromString(userId))
+                );
+    }
+
+    @PutMapping("update/status")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    public ResponseEntity<LinkResponse> updateStatus(
+            @Valid @NotNull @RequestBody LinkUpdateStatusRequest request,
+            @NotEmpty @HeaderParam("X-USER-ID") String userId) {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(
+                        linkService.updateStatus(request, UUID.fromString(userId))
+                );
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    public ResponseEntity<LinkResponse> details(
+            @NotNull @RequestBody Long id,
+            @NotEmpty @HeaderParam("X-USER-ID") String userId) {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(
+                        linkService.details(id, UUID.fromString(userId))
+                );
+    }
+
+    @GetMapping("/user/links")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    public ResponseEntity<List<LinkResponse>> userLinks(
+            @NotEmpty @HeaderParam("X-USER-ID") String userId) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(
+                        linkService.userLinks(UUID.fromString(userId))
+                );
+    }
+}
---
+++ links-service/src/main/java/ir/aspireapps/linker/linksservice/service/LinkService.java
@@ -0,0 +1,100 @@
+package ir.aspireapps.linker.linksservice.service;
+
+import ir.aspireapps.linker.linksservice.dto.LinkRegisterRequest;
+import ir.aspireapps.linker.linksservice.dto.LinkResponse;
+import ir.aspireapps.linker.linksservice.dto.LinkUpdateStatusRequest;
+import ir.aspireapps.linker.linksservice.model.Link;
+import ir.aspireapps.linker.linksservice.model.LinkStatus;
+import ir.aspireapps.linker.linksservice.repository.LinkRepository;
+import ir.aspireapps.linker.linksservice.utility.LinkShortener;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class LinkService {
+    private final LinkRepository linksRepository;
+    private final LinkShortener linkShortener;
+
+    @Transactional
+    public LinkResponse register(@Valid @NotNull LinkRegisterRequest request,
+                                 @NotNull UUID userId) {
+        Link newLink = Link.builder()
+                .title(request.title())
+                .originalUrl(request.url())
+                .shortUrl(linkShortener.shortOf(request.url()))
+                .userId(userId)
+                .expiresAt(request.expiresAt())
+                .build();
+        newLink = linksRepository.save(newLink);
+        return LinkResponse.builder()
+                .id(newLink.getId())
+                .title(newLink.getTitle())
+                .originalUrl(newLink.getOriginalUrl())
+                .shortUrl(newLink.getShortUrl())
+                .userId(newLink.getUserId())
+                .status(newLink.getStatus())
+                .createdAt(newLink.getCreatedAt())
+                .updatedAt(newLink.getUpdatedAt())
+                .expiresAt(newLink.getExpiresAt())
+                .build();
+
+    }
+
+    @Transactional
+    public LinkResponse updateStatus(
+            @Valid @NotNull LinkUpdateStatusRequest request,
+            @NotNull UUID userId) {
+
+        Link link = linksRepository.findByIdAndUserId(request.id(), userId)
+                .orElseThrow(() -> new RuntimeException("Link not found"));
+        if (link.getExpiresAt().isBefore(Instant.now()))
+            link.setStatus(LinkStatus.EXPIRED);
+        else
+            link.setStatus(request.status());
+        return LinkResponse.builder()
+                .id(link.getId())
+                .title(link.getTitle())
+                .originalUrl(link.getOriginalUrl())
+                .shortUrl(link.getShortUrl())
+                .userId(link.getUserId())
+                .status(link.getStatus())
+                .createdAt(link.getCreatedAt())
+                .updatedAt(link.getUpdatedAt())
+                .expiresAt(link.getExpiresAt())
+                .build();
+    }
+
+    @Transactional
+    public LinkResponse details(
+            @NotNull long id,
+            @NotNull UUID userId) {
+        Link link = linksRepository.findByIdAndUserId(id, userId)
+                .orElseThrow(() -> new RuntimeException("Link not found"));
+        if (link.getExpiresAt().isBefore(Instant.now()))
+            link.setStatus(LinkStatus.EXPIRED);
+        return LinkResponse.builder()
+                .id(link.getId())
+                .title(link.getTitle())
+                .originalUrl(link.getOriginalUrl())
+                .shortUrl(link.getShortUrl())
+                .userId(link.getUserId())
+                .status(link.getStatus())
+                .createdAt(link.getCreatedAt())
+                .updatedAt(link.getUpdatedAt())
+                .expiresAt(link.getExpiresAt())
+                .build();
+    }
+
+    public List<LinkResponse> userLinks(UUID userId) {
+        return linksRepository.findUserLinks(userId);
+    }
+}
