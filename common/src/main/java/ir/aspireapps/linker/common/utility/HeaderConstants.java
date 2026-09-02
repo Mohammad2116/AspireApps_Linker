84ecfa4634bbfae7fc7c561b302c46450c63232b
Mohammad Momensafaie <Mohammad2116@gmail.com>
2026-09-02 11:51:51 +0330

    feat - add HeaderConstants and ClaimConstants
    Signed-off-by: Mohammad Momensafaie <Mohammad2116@gmail.com>

---
+++ common/src/main/java/ir/aspireapps/linker/common/utility/HeaderConstants.java
@@ -0,0 +1,11 @@
+package ir.aspireapps.linker.common.utility;
+
+public final class HeaderConstants {
+    public static final String X_USERNAME = "X_USER_NAME";
+    public static final String X_USER_ID = "X_USER_ID";
+    public static final String X_USER_STATE = "X_USER_STATE";
+    public static final String X_USER_ROLES = "X_USER_ROLES";
+
+    private HeaderConstants() {
+    }
+}
