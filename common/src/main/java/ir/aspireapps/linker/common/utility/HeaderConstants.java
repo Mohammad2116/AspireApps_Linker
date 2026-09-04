e42a7cf90d2016ae123f4350c7f864f96eff9ced
Mohammad Momensafaie <Mohammad2116@gmail.com>
2026-09-04 17:47:00 +0330

    fix: value of X_USER_NAME to X_USERNAME
    Signed-off-by: Mohammad Momensafaie <Mohammad2116@gmail.com>

--- common/src/main/java/ir/aspireapps/linker/common/utility/HeaderConstants.java
+++ common/src/main/java/ir/aspireapps/linker/common/utility/HeaderConstants.java
@@ -1,7 +1,7 @@
 package ir.aspireapps.linker.common.utility;
 
 public final class HeaderConstants {
-    public static final String X_USERNAME = "X_USER_NAME";
+    public static final String X_USERNAME = "X_USERNAME";
     public static final String X_USER_ID = "X_USER_ID";
     public static final String X_USER_STATUS = "X_USER_STATE";
     public static final String X_USER_ROLES = "X_USER_ROLES";
