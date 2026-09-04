8347fdb36e6587bd1fdef26038c5a20fef48d4a3
Mohammad Momensafaie <Mohammad2116@gmail.com>
2026-09-04 17:31:31 +0330

    test: add 'http://user-service/' to refresh redirection paths and remove 'ir/aspireapps/'
    Signed-off-by: Mohammad Momensafaie <Mohammad2116@gmail.com>

--- gateway-server/src/main/java/ir/aspireapps/linker/gatewayserver/config/JwtAuthenticationFilter.java
+++ gateway-server/src/main/java/ir/aspireapps/linker/gatewayserver/config/JwtAuthenticationFilter.java
@@ -146,7 +146,7 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
                 .refreshToken(refreshToken)
                 .returnUrl(exchange.getRequest().getURI().getRawPath())
                 .build();
-        String redirectPath = "/ir/aspireapps/linker/auth/web/v1/refresh";
+        String redirectPath = "http://user-service/linker/auth/web/v1/refresh";
         return webClientBuilder
                 .build()
                 .post()
