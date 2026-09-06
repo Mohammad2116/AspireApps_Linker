852992ffd5a1c19bc00679cd444f14e5e82ebd8e
Mohammad Momensafaie <Mohammad2116@gmail.com>
2026-09-06 10:27:22 +0330

    feat: add Kafka topics name as constants

---
+++ common/src/main/java/ir/aspireapps/linker/common/utility/KafkaTopicsConstants.java
@@ -0,0 +1,12 @@
+package ir.aspireapps.linker.common.utility;
+
+public final class KafkaTopicsConstants {
+    public static final String LINK_VISIT_TOPIC = "link-visit-topic";
+    public static final String POPULARITY_RESPONSE_TOPIC = "popularity-response-topic";
+    public static final String link_registered_topic = "link-registered-topic";
+    public static final String LINK_DELETED_TOPIC = "link-deleted-topic";
+    public static final String ANALYTICS_ERROR_TOPIC = "analytics-error-topic";
+
+    private KafkaTopicsConstants() {
+    }
+}
