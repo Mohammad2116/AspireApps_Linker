4f40d57a4666cba1af8a71ad28ee832dd90d8b61
Mohammad Momensafaie <Mohammad2116@gmail.com>
2026-09-06 18:25:03 +0330

    feat: add LocalTimeZone Calculated in expiration date time
    Signed-off-by: Mohammad Momensafaie <Mohammad2116@gmail.com>

--- user-service/src/main/java/ir/aspireapps/linker/userservice/form/AddLinkForm.java
+++ user-service/src/main/java/ir/aspireapps/linker/userservice/form/AddLinkForm.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
-import java.time.Instant;
+import java.time.LocalDateTime;
 
 @Data
 public class AddLinkForm {
@@ -20,5 +20,5 @@ public class AddLinkForm {
     @NotNull
     private boolean status;
     @NotNull
-    private Instant expiresAt;
+    private LocalDateTime expiresAt;
 }
