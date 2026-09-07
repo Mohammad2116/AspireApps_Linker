5987d1c9f20e571575439eae166a0a4003ab247a
Mohammad Momensafaie <Mohammad2116@gmail.com>
2026-09-07 14:49:49 +0330

    feat: now business errors creates an outbox message on kafka
    Signed-off-by: Mohammad Momensafaie <Mohammad2116@gmail.com>

--- analysis-service/src/main/java/ir/aspireapps/linker/analysisservice/repository/AnalysisRepository.java
+++ analysis-service/src/main/java/ir/aspireapps/linker/analysisservice/repository/AnalysisRepository.java
@@ -5,10 +5,12 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AnalysisRepository extends JpaRepository<AnalyzeData, Long> {
     boolean existsByShortedUrl(@NotEmpty @Size(min = 4, max = 10) String shortedUrl);
 
-    AnalyzeData findByShortedUrl(String shortedUrl);
+    Optional<AnalyzeData> findByShortedUrl(String shortedUrl);
 
     void deleteByShortedUrl(String shortedUrl);
 }
