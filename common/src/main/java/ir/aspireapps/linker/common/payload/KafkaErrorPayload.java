8e8b144f6d21031f0d44c6398d2a73441a440d30
Mohammad Momensafaie <Mohammad2116@gmail.com>
2026-09-07 14:51:32 +0330

    feat: add builder and all args constractor to KafkaErrorPayload
    Signed-off-by: Mohammad Momensafaie <Mohammad2116@gmail.com>

--- common/src/main/java/ir/aspireapps/linker/common/payload/KafkaErrorPayload.java
+++ common/src/main/java/ir/aspireapps/linker/common/payload/KafkaErrorPayload.java
@@ -1,7 +1,10 @@
 package ir.aspireapps.linker.common.payload;
 
+import lombok.Builder;
+
 import java.time.Instant;
 
+@Builder
 public record KafkaErrorPayload(
         Instant timestamp,
         int status,
@@ -11,4 +14,13 @@ public record KafkaErrorPayload(
         String aggregateId,
         String sourceTopic
 ) {
+    public KafkaErrorPayload(Instant timestamp, int status, String code, String error, String message, String aggregateId, String sourceTopic) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.code = code;
+        this.error = error;
+        this.message = message;
+        this.aggregateId = aggregateId;
+        this.sourceTopic = sourceTopic;
+    }
 }
