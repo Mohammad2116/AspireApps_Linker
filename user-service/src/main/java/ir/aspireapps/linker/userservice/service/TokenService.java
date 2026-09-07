f773d7eb16331ef8777df8cc05fda54f5aafd7fa
Mohammad Momensafaie <Mohammad2116@gmail.com>
2026-09-07 09:56:13 +0330

    fix: minor changes at hashToken service
    Signed-off-by: Mohammad Momensafaie <Mohammad2116@gmail.com>

--- user-service/src/main/java/ir/aspireapps/linker/userservice/service/TokenService.java
+++ user-service/src/main/java/ir/aspireapps/linker/userservice/service/TokenService.java
@@ -4,6 +4,7 @@ import ir.aspireapps.linker.common.utility.LoggingEvents;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -22,7 +23,7 @@ public class TokenService {
     public String hashToken(String token) {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] hash = digest.digest(token.getBytes());
+            byte[] hash = digest.digest(token.getBytes(StandardCharsets.UTF_8));
             return Base64.getUrlEncoder().withoutPadding().encodeToString(hash);
         } catch (NoSuchAlgorithmException e) {
             log.error("{} - Hash Algorithm Not Found", LoggingEvents.INTERNAL_SERVER_ERROR, e);
